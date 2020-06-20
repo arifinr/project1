df60d8d28048a2ebf428e69ca048a8fbc6aa0566
Arifin Rahman <arifinrahman6@gmail.com>
2020-06-20 18:26:20 -0400

    Initialized user database models

---
+++ database.py
@@ -0,0 +1,22 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(autocommit=False, autoflush=False,bind=engine))
+
+Base = declarative_base()
+Base.query = db.query_property()
+
+def init_db():
+    # import all modules here that might define models so that
+    # they will be registered properly on the metadata.  Otherwise
+    # you will have to import them first before calling init_db()
+    import models
+    Base.metadata.create_all(bind=engine)
